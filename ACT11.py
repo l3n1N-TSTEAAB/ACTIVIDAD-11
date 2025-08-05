77f53061aab736aae363131ce7615e278f93c881
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-04 19:40:19 -0600

    QUICK_SORT FINAL

--- ACT11.py
+++ ACT11.py
@@ -5,25 +5,21 @@ def quick_sort(lista):
 
     pivote = lista[0]
     menores = [x for x in lista[1:] if x<pivote]
-    iguales = [x for x in lista[1:] if x==pivote]
+    iguales = [x for x in lista if x==pivote]
     mayores = [x for x in lista[1:] if x>pivote]
 
     return quick_sort(menores)+iguales+quick_sort(mayores)
 
-
-
-
-
-
 print("ORDENAMIENTOS DE DATOS")
 lista = []
-cantidad = int(input("CANTIDAD DE NOMBRES A INGRESAR"))
+cantidad = int(input("CANTIDAD DE NOMBRES A INGRESAR: "))
 contador = 1
 
-while(contador <= cantidad):
+while contador <= cantidad:
     elemento=input("INGRESO DE NOMBRE: ")
     lista.append(elemento)
     contador += 1
 
 
 resultado = quick_sort(lista)
+print(resultado)
